e6da6a6d68afb7ea0e1a0a4ce6f5c4ae2b45a50e
zarei.dev <mohammad.zarei1380@gmail.com>
2022-07-11 12:10:54 +0430

    feat: Add footer

---
+++ admin/views/components/footer.php
@@ -0,0 +1,23 @@
+<div class="pod-footer">
+    <div class="pod-footer-links">
+        <ul>
+            <li><a href="https://podro.com/contact-us/" target="_blank" rel="noopener noreferrer"><?php echo esc_html_e( 'Support', POD_TEXTDOMAIN ) ?> <span class="dashicons dashicons-external"></span></a></li>
+            <li><a href="mailto:hi@podro.com" target="_blank" rel="noopener noreferrer"><?php echo esc_html_e( 'Email Us', POD_TEXTDOMAIN ) ?> <span class="dashicons dashicons-email"></span></a></li>
+			<li><a href="https://podro.com/terms-and-conditions/" target="_blank" rel="noopener noreferrer"><?php echo esc_html_e( 'Terms and Conditions', POD_TEXTDOMAIN ) ?> <span class="dashicons dashicons-privacy"></span></a></li>
+        </ul>
+    </div>
+    <div class="pod-footer-social">
+        <div class="footer__social-icons">
+
+            <a class="footer__social-icons__link footer__social-icons--linkedin" href="<?php echo esc_url(get_locale() == 'fa_IR' ? 'https://www.linkedin.com/company/podroco/' : 'https://www.linkedin.com/company/podroco/') ?>" target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29.41 29.85"><g><g><rect x="0.97" y="10.18" width="6.53" height="19.16" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.00622773842599px"></rect><path d="M28.9,16.17V29.34H22.38V17.53a2.43,2.43,0,0,0-1.2-2.13A3.09,3.09,0,0,0,19.6,15a2.79,2.79,0,0,0-1.29.34,3.62,3.62,0,0,0-1.91,3.26V29.34H9.87V10.18H16.4v2.05A8,8,0,0,1,21.09,10a11.27,11.27,0,0,1,3,.28h0l.24.07a8,8,0,0,1,.78.25l.23.1.22.09.24.13a5.64,5.64,0,0,1,.7.46l0,0A6,6,0,0,1,28.9,16.17Z" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.00622773842599px"></path><circle cx="4.42" cy="4.42" r="3.92" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.00622773842599px"></circle></g></g></svg>
+            </a>
+            <a class="footer__social-icons__link footer__social-icons--twitter" href="<?php echo esc_url(get_locale() == 'fa_IR' ? 'https://twitter.com/PodroCo' : 'https://twitter.com/PodroCo') ?>" target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34.72 28.86"><g><g><path d="M8.81,23.82a7.45,7.45,0,0,1-2.72,1.94,10.49,10.49,0,0,1-2.5.83A5.9,5.9,0,0,1,1,26.78c11.19,4.6,28.58-.61,28.66-19.15,0,0,3.68-2.07,4.59-4.68L31.68,4,33.45.66a8.5,8.5,0,0,1-5.59,2.22C26.17-.95,15-1,16.67,9.31,8.32,10.31,1.35,2.5,1.35,2.5S-.49,7.78,3.87,11c0,0-.42,1.13-3.37,0,0,0,.92,5.9,6,5.9,0,0,.41,2.74-3.3,1.91A7.67,7.67,0,0,0,9.54,22a7.23,7.23,0,0,1-.17.8A3.68,3.68,0,0,1,8.81,23.82Z" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.00622773842599px"></path></g></g></svg>
+            </a>
+            <a class="footer__social-icons__link footer__social-icons--instagram" href="<?php echo esc_url(get_locale() == 'fa_IR' ? 'https://www.instagram.com/podroco/' : 'https://www.instagram.com/podroco/') ?>" target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29.2 29.34"><g><g><rect x="0.5" y="0.5" width="28.19" height="28.34" rx="8.18" ry="8.18" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.00622773842599px"></rect><circle cx="14.6" cy="14.67" r="6.8" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.00622773842599px"></circle><circle cx="22.61" cy="6.65" r="1.22" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.00622773842599px"></circle></g></g></svg>
+            </a>
+        </div>
+    </div>
+</div>
