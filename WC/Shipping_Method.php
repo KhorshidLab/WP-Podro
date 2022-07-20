557e87f459df490609616556a0ca9be36f9c52a9
zarei.dev <mohammad.zarei1380@gmail.com>
2022-07-21 01:19:05 +0430

    feat: Add price to shipping method

--- WC/Shipping_Method.php
+++ WC/Shipping_Method.php
@@ -39,12 +39,27 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 							'description' 	=> __( 'This controls the title which the user sees during checkout.', POD_TEXTDOMAIN ),
 							'default'		=> __( 'Podro', POD_TEXTDOMAIN ),
 							'desc_tip'		=> true
+						),
+						'price' => array(
+							'title' 		=> __( 'هزینه ارسال', POD_TEXTDOMAIN ),
+							'type' 			=> 'number',
+							'description' 	=> __( 'هزینه دریافتی از کاربر', POD_TEXTDOMAIN ),
+							'default'		=> __( '1000', POD_TEXTDOMAIN ),
+							'desc_tip'		=> true
 						)
 					);
 					$this->enabled              = $this->get_option( 'enabled' );
 					$this->title                = $this->get_option( 'title' );
 
 					add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
+				}
+
+				public function calculate_shipping( $package = array() ) {
+					$this->add_rate( array(
+						'id'    => $this->id . $this->instance_id,
+						'label' => $this->title,
+						'cost'  => $this->get_option( 'price' ),
+					) );
 				}
 			}
 		}
